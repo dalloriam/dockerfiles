c68f50483adb6ff463e36204942d980037467c5d
Dalloriam <dalloriam@gmail.com>
2019-02-18 19:19:31 -0500

    Improve ORC support

--- worktimer/worktimer.py
+++ worktimer/worktimer.py
@@ -5,6 +5,8 @@ from http import HTTPStatus
 
 from time import sleep
 
+from typing import Any, Dict
+
 import os
 import requests
 import subprocess
@@ -17,12 +19,33 @@ OS_TYPE = os.getenv('HOST_OS', 'linux-gnu')
 ORC_HOST = 'docker.for.mac.host.internal' if OS_TYPE.startswith('darwin') else 'localhost'
 
 
-def send_message(title: str, message: str) -> None:
-    # TODO: Enable only if ORC supports notifications on this machine.
-    resp = requests.post(f'http://{ORC_HOST}:{ORC_PORT}/local/notify', json={'title': title, 'message': message})
+class FeatureNotSupported(Exception):
+    pass
+
+
+def _supports(module: str, action: str) -> bool:
+    resp = requests.post(f'http://{ORC_HOST}:{ORC_PORT}/manage/actions_available')
+    out = resp.json()
+    return module in out and action in out[module]
+
+# TODO: Move to proper ORC client integrated to stdlib.
+def orc(act_tag: str, body: Dict[str, Any] = None) -> Dict[str, Any]:
+    mod_name, act_name = act_tag.split('/')
+
+    # Check if feature is supported by ORC
+    if not _supports(mod_name, act_name):
+        raise FeatureNotSupported(act_tag)
+
+    resp = requests.post(f'http://{ORC_HOST}:{ORC_PORT}/{mod_name}/{act_name}', json=body)
 
     if resp.status_code != HTTPStatus.OK:
         raise ValueError(resp.text)
+    
+    return resp.json()
+
+
+def send_message(title: str, message: str) -> None:
+    orc('local/notify', {'title': title, 'message': message})
 
 
 class WorkTimer:
